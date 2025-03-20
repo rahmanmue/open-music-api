e8d12d2cf01fce238b85277f7ecd444109d127ac
rahmanmue <rahman.muraman@gmail.com>
2025-03-20 21:05:46 +0700

    feat: add mapdb to playlists activities

--- src/utils/index.js
+++ src/utils/index.js
@@ -34,9 +34,55 @@ const mapDBSongsToModel = ({
   albumId,
 }) => ({ id, title, year, performer, genre, duration, albumId });
 
+const mapDBUsersToModel = ({ id, username, password, fullname }) => ({
+  id,
+  username,
+  password,
+  fullname,
+});
+
+const mapDBPlaylistsToModel = ({ playlistId, name, username }) => ({
+  id: playlistId,
+  name,
+  username,
+});
+
+const mapDBPlaylistSongsToModel = (rows) => {
+  const { id, name, username } = rows[0];
+
+  const songs = rows
+    .filter(({ title }) => title !== null)
+    // eslint-disable-next-line camelcase
+    .map(({ song_id, title, performer }) => ({
+      // eslint-disable-next-line camelcase
+      id: song_id,
+      title,
+      performer,
+    }));
+
+  return { id, name, username, songs };
+};
+
+const mapDBPlaylistActivitiesToModel = (rows) => {
+  console.log(rows);
+  const { playlistId } = rows[0];
+  const activities = rows.map(({ username, title, action, time }) => ({
+    username,
+    title,
+    action,
+    time,
+  }));
+
+  return { playlistId, activities };
+};
+
 module.exports = {
   mapDBAlbumToModel,
   mapDBAlbumSongToModel,
   mapDBSongToModel,
   mapDBSongsToModel,
+  mapDBUsersToModel,
+  mapDBPlaylistsToModel,
+  mapDBPlaylistSongsToModel,
+  mapDBPlaylistActivitiesToModel,
 };
